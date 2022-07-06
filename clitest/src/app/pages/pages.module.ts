a527a26a9b06a1a9ba8c4d26ca3558848e5cfbce
juanvaleriomayta <juanvalerio785@gmail.com>
2022-07-05 23:56:30 -0500

    Fin sección 8

--- clitest/src/app/pages/pages.module.ts
+++ clitest/src/app/pages/pages.module.ts
@@ -12,6 +12,8 @@ import { PagesComponent } from './pages.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProgressComponent } from './progress/progress.component';
 import { AccountSettingsComponent } from './account-settings/account-settings.component';
+import { PromesasComponent } from './promesas/promesas.component';
+import { RxjsComponent } from './rxjs/rxjs.component';
 
 
 
@@ -22,6 +24,8 @@ import { AccountSettingsComponent } from './account-settings/account-settings.co
     DashboardComponent,
     ProgressComponent,
     AccountSettingsComponent,
+    PromesasComponent,
+    RxjsComponent,
   ],
   imports: [
     CommonModule,
--- clitest/src/app/shared/breadcrumbs/breadcrumbs.component.ts
+++ clitest/src/app/shared/breadcrumbs/breadcrumbs.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { filter, map } from 'rxjs/operators';
+import { ActivationEnd, Router } from '@angular/router';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-breadcrumbs',
@@ -6,11 +9,31 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class BreadcrumbsComponent implements OnInit {
+export class BreadcrumbsComponent implements OnDestroy {
 
-  constructor() { }
+  public titulo: string;
 
-  ngOnInit(): void {
+  public tituloSubs$: Subscription;
+
+  constructor(private router: Router) {
+
+
+   this.tituloSubs$ = this.getArgumentosRutas().subscribe(({ titulo }) => {
+      this.titulo = titulo;
+      document.title = `AdminPro - ${titulo}`;
+    });
+  }
+  ngOnDestroy(): void {
+    this.tituloSubs$.unsubscribe();
   }
 
+  getArgumentosRutas() {
+
+    return this.router.events
+      .pipe(
+        filter((event: any) => event instanceof ActivationEnd),
+        filter((event: ActivationEnd) => event.snapshot.firstChild === null),
+        map((event: ActivationEnd) => event.snapshot.data),
+      );
+  }
 }
